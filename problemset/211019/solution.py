b564cd064283535d2a1156ca215bcdc9834c4621
CodeWriter21 <CodeWriter21@gmail.com>
2023-12-17 17:03:10 +0330

    added: Solution for problem 211019 in Python3.

---
+++ problemset/211019/solution.py
@@ -0,0 +1,12 @@
+#!/usr/bin python3
+
+data = {
+    'space': 'blue',
+    'mind': 'yellow',
+    'reality': 'red',
+    'power': 'purple',
+    'time': 'green',
+    'soul': 'orange'
+}
+
+print(data[input()])
